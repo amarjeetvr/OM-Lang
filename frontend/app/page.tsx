5a4e9e6fa8894ed8688b8a7acb1498b0b329622e
amarjeetvr <av457508@gmail.com>
2026-01-20 09:43:48 +0530

    Fix frontend folder - add all files properly

---
+++ frontend/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+     <div className="min-h-screen flex items-center justify-center bg-black">
+      <h1 className="text-4xl font-bold text-green-500">
+        Tailwind is Work fine!
+      </h1>
+    </div>
+  );
+}
